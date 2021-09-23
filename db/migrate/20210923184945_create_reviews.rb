8f15011ddf67a1a2c29cd99a13d426dfe30431e9
Rianna Castro <riannaxcastro@gmail.com>
2021-09-23 08:50:55 -1000

    added migrations

---
+++ db/migrate/20210923184945_create_reviews.rb
@@ -0,0 +1,11 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reviews do |t|
+      t.string :name
+      t.string :discription
+      t.belongs_to :college, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
