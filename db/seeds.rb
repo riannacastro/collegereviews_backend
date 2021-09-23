441010f71d4ef94ce59f2489f930e85d9f830e73
Rianna Castro <riannaxcastro@gmail.com>
2021-09-23 09:00:19 -1000

    college seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+uh_manoa = College.create(name:"University of Manoa", mascot:"Vili the Warrior", location:"Honolulu, Hawaii");
+nau = College.create(name:"Northern Arizona University", mascot:"Louie the Lumberjack", location:"Flagstaff, Arizona");
+gcu = College.create(name:"Grand Canyon University", mascot:"Thunder the Antelope", location:"Phoenix, Arizona");
+asu = College.create(name:"Arizona State University", mascot:"Sparky the Sun Devil", location:"Tempe, Arizona");
+osu = College.create(name:"Oregon State University", mascot:"Benny the Beaver", location:"Corvallis, Oregon");
