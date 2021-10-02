65b580f66016fe40cab03fe332f1046b0c7d0816
Rianna Castro <riannaxcastro@gmail.com>
2021-10-01 14:13:20 -1000

    add update

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,13 +5,30 @@ class ReviewsController < ApplicationController
     end
 
     def show
-        review = Review.all
+        review = Review.find_by_id(params[:id])
         render json: review
     end
 
     def create
         review = Review.new(review_params)
+        if review.save
+            render json: review
+        else
+            render json: {error: "Review not saved."}
+        end
     end
+
+    def update
+        review = Review.find_by_id(params[:id])
+        
+        if review.update(review_params)
+            render json: review
+        else
+            render json: {error: "Review could not be updated."}
+        end
+
+    end
+
 
     private
 
